d9c7d785556270e1f7ee6e8eb2394c335de89f24
hysy <lorenzo_parrotta@outlook.it>
2020-12-07 18:21:02 +0100

    Added Calories Filter and Pagination

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import { BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 import Home from "./Home";
 
 function App() {
   return (
-    <Router>
-        <Route exact path="/" component={Home} />
-    </Router>
+    <Home/>
   );
 
 }
--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,36 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import './App.css';
+import Attribution from "./Attribution";
 import Default from "./Default";
 import Recipe from "./Recipe";
+import Pagination from './Pagination';
 
 function Home() {
-    const APP_ID = "YOUR_APP_ID";
-    const APP_KEY = "YOUR_APP_KEY";
+    const APP_ID = "e497ae7f"; // YOUR_APP_ID
+    const APP_KEY = "c08d5b8e338d6ce4a557e2f618337d5a"; // YOUR_APP_KEY
 
+    // Search
     const [recipes, setRecipes] = useState([]);
     const [search, setSearch] = useState("");
     const [query, setQuery] = useState("");
 
+    // Calories Filter
+    const [filterFrom, setFilterFrom] = useState(0);
+    const [filterTo, setFilterTo] = useState(9000);
+    const [filter, setFilter] = useState(`0-9000`);
+
+    // Pagination
+    const [currentPage, setCurrentPage] = useState(1);
+    const [recipesPerPage] = useState(10);
+
     useEffect(async () => {
         GET_RECIPES();
-    }, [query]);
+    }, [query, filter]);
 
+    // Search
     const GET_RECIPES = async () => {
-        const RESPONCE = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+        const RESPONCE = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&calories=${filter}&to=100`);
         const DATA = await RESPONCE.json();
         setRecipes(DATA.hits);
     };
@@ -29,23 +42,67 @@ function Home() {
     const GET_SEARCH = event => {
         event.preventDefault();
         setQuery(search);
-    }
+    };
+
+    // Calories Filter
+    const UPDATE_FILTER_FROM = event => {
+        event.preventDefault();
+        setFilterFrom(event.target.value);
+    };
+
+    const UPDATE_FILTER_TO = event => {
+        event.preventDefault();
+        setFilterTo(event.target.value);
+    };
+
+    const UPDATE_FILTER = event => {
+        event.preventDefault();
+        setFilter(`${filterFrom}-${filterTo}`);
+    };
+
+    const SHOW_FILTERS = () => {
+        let x = document.getElementById("calories-filter");
+        if (x.style.display === "none") {
+            x.style.display = "flex";
+        } else {
+            x.style.display = "none";
+        }
+    };
+
+    // Pagination
+
+    // Get current recipes
+    const indexOfLastRecipe = currentPage * recipesPerPage;
+    const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage;
+    const currentRecipe = recipes.slice(indexOfFirstRecipe, indexOfLastRecipe);
+
+    // Change page
+    const paginate = pageNumber => setCurrentPage(pageNumber);
 
     if (!(query === "")) {
         return (
-            <div className="App">
+            <div className="Home">
                 <div className="title">
-                    <h1>Recipes Finder</h1>
+                    <a href="/"><h1>Recipes Finder</h1></a>
                 </div>
                 <form onSubmit={GET_SEARCH} className="search-form">
-                    <input className="search-bar" type="text" placeholder="Find Recipes" value={search} onChange={UPDATE_SEARCH} />
+                    <input className="search-bar" type="text" placeholder="Type ingredients separated by spaces" value={search} onChange={UPDATE_SEARCH} />
                     <button className="search-button" type="submit">Search</button>
                 </form>
+                <div className="filters">
+                    <p onClick={SHOW_FILTERS}>Filters</p>
+                    <form onSubmit={UPDATE_FILTER}  id="calories-filter" className="search-form-filters">
+                        <p>Calories: </p>
+                        <input className="search-filter" type="number" step="100" min="0" max="9000" placeholder="From" value={filterFrom} onChange={UPDATE_FILTER_FROM} />
+                        <input className="search-filter" type="number" step="100" min="0" max="9000" placeholder="To" value={filterTo} onChange={UPDATE_FILTER_TO} />
+                        <button className="button-filter" type="submit">Update</button>
+                    </form>
+                </div>
                 <div className="recipe-container">
-                    {recipes.map((recipe) => (
+                    {currentRecipe.map((recipe) => (
                         <Recipe
                             title={recipe.recipe.label}
-                            calories={Math.round(recipe.recipe.calories)}
+                            calories={Math.round((recipe.recipe.calories / recipe.recipe.yield))}
                             image={recipe.recipe.image}
                             key={recipe.recipe.title}
                             url={recipe.recipe.url}
@@ -53,7 +110,12 @@ function Home() {
                         />
                     ))}
                 </div>
-                <div id="edamam-badge" data-color="transparent"></div>
+                <Pagination
+                    recipesPerPage={recipesPerPage}
+                    totalRecipes={recipes.length}
+                    paginate={paginate}
+                />
+                <Attribution></Attribution>
             </div>
 
         );
@@ -61,16 +123,16 @@ function Home() {
         return (
             <div className="App">
                 <div className="title">
-                    <h1>Recipes Finder</h1>
+                    <a href="/"><h1>Recipes Finder</h1></a>
                 </div>
                 <form onSubmit={GET_SEARCH} className="search-form">
-                    <input className="search-bar" type="text" placeholder="Find Recipes" value={search} onChange={UPDATE_SEARCH} />
+                    <input className="search-bar" type="text" placeholder="Type ingredients separated by spaces or a recipe" value={search} onChange={UPDATE_SEARCH} />
                     <button className="search-button" type="submit">Search</button>
                 </form>
                 <div className="recipe-container">
                     <Default />
                 </div>
-                <div id="edamam-badge" data-color="transparent"></div>
+                <Attribution></Attribution>
             </div>
         )
     }
